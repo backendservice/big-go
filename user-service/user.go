dbe9c1f5d06dd30b3e854c1d038b2144814ec3c2
Hojoon Lee <ethos93@gmail.com>
2018-06-26 17:22:31 +0900

    modified

--- user-service/user.go
+++ user-service/user.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"math"
 	"net"
+	"strings"
 
 	pb "github.com/backendservice/big-go/big"
 	"google.golang.org/grpc"
@@ -16,16 +18,47 @@ const (
 
 type server struct{}
 
+var users []*pb.UserRequest
+
 func (s *server) Hello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
 	return &pb.HelloResponse{Message: "Hello " + in.Name}, nil
 }
 
 func (s *server) RegistUser(ctx context.Context, in *pb.UserRequest) (*pb.UserResponse, error) {
-	return &pb.UserResponse{Message: "Hello " + in.Name}, nil
+	users = append(users, in)
+	return &pb.UserResponse{Message: "User (" + in.Name + ") register success"}, nil
 }
 
 func (s *server) FindUser(ctx context.Context, in *pb.FindRequest) (*pb.FindResponse, error) {
-	return &pb.FindResponse{Message: "Hello " + in.Religion}, nil
+	filtereduser, count := Filter(in)
+	return &pb.FindResponse{Count: count, Result: filtereduser, Message: "Hello " + in.Religion}, nil
+}
+
+func Filter(condition *pb.FindRequest) ([]*pb.UserRequest, int32) {
+	var tmpuser []*pb.UserRequest
+	var count int32
+	for i := 0; i < len(users); i++ {
+		if condition.GetStartage() < users[i].Age && condition.GetEndage() > users[i].Age &&
+			condition.GetStartlatitude() < users[i].Latitude && condition.GetEndlatitude() > users[i].Latitude &&
+			condition.GetStartlongitude() < users[i].Longitude && condition.GetEndlongitude() > users[i].Longitude &&
+			strings.Compare(condition.GetGender(), users[i].Gender) == 0 &&
+			strings.Compare(condition.GetReligion(), users[i].Religion) == 0 &&
+			strings.Compare(condition.GetNationality(), users[i].Nationality) == 0 {
+			tmpuser = append(tmpuser, users[i])
+			count++
+		}
+	}
+	return tmpuser, count
+}
+
+func CheckDistance(distance int32, latitude1 float64, latitude2 float64, longitude1 float64, longitude2 float64) bool {
+
+	var dis = math.Acos(math.Sin(latitude1)*math.Sin(latitude2) + math.Cos(latitude1)*math.Cos(latitude2)*math.Cos(longitude1-longitude2))
+
+	if dis > float64(distance) {
+		return false
+	}
+	return true
 }
 
 func main() {
