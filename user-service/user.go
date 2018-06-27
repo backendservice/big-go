052e5133f8a6e343c30e0403f89956eae61c965e
Hojoon Lee <40584543+ethos93@users.noreply.github.com>
2018-06-27 16:45:13 +0900

    Update user.go

--- user-service/user.go
+++ user-service/user.go
@@ -30,18 +30,18 @@ func (s *server) RegistUser(ctx context.Context, in *pb.UserRequest) (*pb.UserRe
 }
 
 func (s *server) FindUser(ctx context.Context, in *pb.FindRequest) (*pb.FindResponse, error) {
-	filtereduser, count := Filter(in)
+	filtereduser, count := filter(in)
 	return &pb.FindResponse{Count: count, Result: filtereduser, Message: "Hello " + in.Religion, Code: 200}, nil
 }
 
-func Filter(condition *pb.FindRequest) ([]*pb.UserRequest, int32) {
+func filter(condition *pb.FindRequest) ([]*pb.UserRequest, int32) {
 	var tmpuser []*pb.UserRequest
 	var count int32
 	for i := 0; i < len(users); i++ {
 		if (strings.Compare(condition.GetAgeType(), "") == 0 || strings.Compare(condition.GetAgeType(), "E") == 0 && condition.GetAge() == users[i].Age ||
 			strings.Compare(condition.GetAgeType(), "U") == 0 && condition.GetAge() < users[i].Age ||
 			strings.Compare(condition.GetAgeType(), "D") == 0 && condition.GetAge() > users[i].Age) &&
-			(condition.GetDistance() == 0 || CheckDistance(condition.GetDistance(), float64(condition.GetLatitude()), float64(users[i].Latitude), float64(condition.GetLongitude()), float64(users[i].Longitude))) &&
+			(condition.GetDistance() == 0 || checkDistance(condition.GetDistance(), float64(condition.GetLatitude()), float64(users[i].Latitude), float64(condition.GetLongitude()), float64(users[i].Longitude))) &&
 			(strings.Compare(condition.GetGender(), "") == 0 || strings.Compare(condition.GetGender(), users[i].Gender) == 0) &&
 			(strings.Compare(condition.GetReligion(), "") == 0 || strings.Compare(condition.GetReligion(), users[i].Religion) == 0) &&
 			(strings.Compare(condition.GetNationality(), "") == 0 || strings.Compare(condition.GetNationality(), users[i].Nationality) == 0) {
@@ -52,7 +52,7 @@ func Filter(condition *pb.FindRequest) ([]*pb.UserRequest, int32) {
 	return tmpuser, count
 }
 
-func CheckDistance(distance int32, latitude1 float64, latitude2 float64, longitude1 float64, longitude2 float64) bool {
+func checkDistance(distance int32, latitude1 float64, latitude2 float64, longitude1 float64, longitude2 float64) bool {
 
 	var dis = math.Acos(math.Sin(latitude1)*math.Sin(latitude2) + math.Cos(latitude1)*math.Cos(latitude2)*math.Cos(longitude1-longitude2))
 
